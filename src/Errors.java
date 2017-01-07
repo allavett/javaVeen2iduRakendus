456af10bf437a7732970686648ebdbd0a7610d29
Allar Vendla <allarvendla@gmail.com>
2017-01-08 01:45:44 +0200

    Reorganize error messages.

---
+++ src/Errors.java
@@ -0,0 +1,12 @@
+/**
+ * Created by AllarVendla on 08.01.2017.
+ */
+public class Errors {
+    private static String errors;
+    public static void setErrors(String err){
+        errors = err;
+    }
+    public static String getErrors(){
+        return errors;
+    }
+}
